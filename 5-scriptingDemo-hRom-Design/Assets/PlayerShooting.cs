3ae50c75184f028d89b8cb6862c1859eca491f2a
Hannah R. <156931522+hRom-Design@users.noreply.github.com>
2024-04-29 13:30:28 -0400

    4/29

---
+++ 5-scriptingDemo-hRom-Design/Assets/PlayerShooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject shootPoint;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject clone = Instantiate(prefab);
+            clone.transform.position = shootPoint.transform.position;
+            clone.transform.rotation = shootPoint.transform.rotation;
+        }
+    }
+}
---
+++ 5-scriptingDemo-hRom-Design/Assets/WaveSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public float startTime;
+    public float endTime;
+    public float spawnRate;
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", startTime, spawnRate);
+        Invoke("CancelInvoke", endTime);
+    }
+    void Spawn()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        
+    }
+}
